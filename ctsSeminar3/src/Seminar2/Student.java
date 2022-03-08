6b7c1ce08dbaab640c5e5118d76242089703c57e
bpapatraian <93181108+bpapatraian@users.noreply.github.com>
2022-03-08 18:37:20 +0200

    Add files via upload

---
+++ ctsSeminar3/src/Seminar2/Student.java
@@ -0,0 +1,27 @@
+package Seminar2;
+
+public class Student extends Aplicant {
+	protected String facultate;
+	protected int anStudii;
+
+	public Student() {
+		super();
+	}
+
+	public void setFacultate(String facultate) {
+		this.facultate = facultate;
+	}
+
+	public void setAnStudii(Integer anStudii) {
+		this.anStudii = anStudii;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder("{Angajat: ");
+		sb.append(super.toString());
+		sb.append("Facultate=").append(facultate);
+		sb.append(", An studii=").append(anStudii);
+		return sb.toString();
+	}
+}
